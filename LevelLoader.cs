0d7af605f4b59418732d6c54f4a9acfef9235e98
Jamal Hussain <40926607+JHussain-Jimmy@users.noreply.github.com>
2021-02-18 12:30:39 +0100

    Add files via upload
    Scene Managment, Loading Screen, Randomly play next scene code etc

---
+++ LevelLoader.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject Question0;
+    public GameObject Question1;
+    public GameObject Question2;
+    public GameObject Question3;
+    public GameObject Question4;
+    public GameObject Question5;
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text ProgressText;
+    private int sceneIndex;
+    static public bool[] answer = new bool[7];
+
+    public void Start()
+    {
+        Time.timeScale = 1f;
+        SceneValue.scenevalue[SceneValue.sceneplayed] = true;
+        if (SceneValue.sceneplayed == 0)
+        {
+            if (! allScenesPlayed() )
+            {
+                RunScene();
+            }
+            else
+                Question0.SetActive(true);
+        }
+        else if (SceneValue.sceneplayed == 1)
+        {
+            Question1.SetActive(true);
+        }
+        else if (SceneValue.sceneplayed == 2)
+        {
+            Question2.SetActive(true);
+        }
+        else if (SceneValue.sceneplayed == 3)
+        {
+            Question3.SetActive(true);
+        }
+        else if (SceneValue.sceneplayed == 4)
+        {
+            Question4.SetActive(true);
+        }
+        else if (SceneValue.sceneplayed == 5 || SceneValue.sceneplayed == 6)
+        {
+            Question5.SetActive(true);
+        }
+    }
+
+    public void answeredyes()
+    {
+        answer[SceneValue.sceneplayed] = true;
+        RunScene();
+    }
+    public void answeredno()
+    {
+        answer[SceneValue.sceneplayed] = false;
+        RunScene();
+    }
+
+    public void RunScene()
+    {
+        Question1.SetActive(false);
+        Question2.SetActive(false);
+        Question3.SetActive(false);
+        Question4.SetActive(false);
+        Question5.SetActive(false);
+        if (!allScenesPlayed())
+        {
+            sceneIndex = randomizeNextScene();
+            StartCoroutine(LoadAsynchronously(sceneIndex));
+        }
+        else
+        {
+            SceneValue.CreateText();
+            Question0.SetActive(true);
+        }
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progress = Mathf.Round(progress * 100f) / 100f;
+            ProgressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+
+    public void overnowprint()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+
+    public bool allScenesPlayed()
+    {
+      
+      for (int i = 0; i < SceneValue.scenevalue.Length; i++)
+        {
+           if (SceneValue.scenevalue[i] == false)
+                return false;
+        }
+        return true;
+    }
+
+    public int randomizeNextScene()
+    {
+         int unplayedScenes = 0;
+         int [] tempscenesIndexes = new int[SceneValue.scenevalue.Length];
+         for (int i = 0; i < SceneValue.scenevalue.Length; i++)
+         {
+             if (SceneValue.scenevalue[i] == false)
+             {
+                 tempscenesIndexes[unplayedScenes] = i;
+                 unplayedScenes++;
+             }
+         }
+         int [] unplayedscenesIndexes = new int[unplayedScenes];
+
+         for (int j = 0; j < unplayedScenes; j++)
+         {
+             unplayedscenesIndexes[j] = tempscenesIndexes[j];
+         }
+         sceneIndex = Random.Range(unplayedscenesIndexes[0], unplayedscenesIndexes[unplayedScenes - 1] + 1);
+
+         if (unplayedScenes > 2)
+         {
+             if (SceneValue.sceneplayed % 2 == 0)
+             {
+                 if (sceneIndex == SceneValue.sceneplayed - 1)
+                     randomizeNextScene();
+             }
+             else
+             {
+                 if (sceneIndex == SceneValue.sceneplayed + 1)
+                     randomizeNextScene();
+             }
+         }
+        if (sceneIndex == SceneValue.sceneplayed)
+            randomizeNextScene();
+        return sceneIndex;
+    }
+}
---
+++ SceneValue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+public class SceneValue : MonoBehaviour
+{
+    static public bool[] scenevalue = new bool[7];
+    static public int sceneplayed;
+
+    public void Start ()
+    { 
+        sceneplayed = SceneManager.GetActiveScene().buildIndex;
+       // if (scenevalue [scenevalue.Length-2] == true)
+       //CreateText();
+    }
+    public void PlayGame()
+    {
+        //sceneplayed = SceneManager.GetActiveScene().buildIndex;
+        // It is for first time click from UI menu
+        scenevalue[sceneplayed] = true;
+        SceneManager.LoadScene("LoadingScene");
+    }
+    static public void CreateText()
+    {
+        string[] question = new string[] { "\n",
+                                         "SIMPLE YIELD with Conventional Car",
+                                         "SIMPLE YIELD with Autonomous Car", 
+                                         "PARKING-SPOT-Yield with Conventional Car",
+                                         "PARKING-SPOT-Yield with Autonomous Car",
+                                         "Pass-or-Wait with Conventional Car",
+                                         "Pass-or-Wait with Autonomous Car"};
+        string answer;
+        string path = Application.dataPath + "/Log File.txt";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "ProSocial Experiment User Data:\n\n");
+        }
+        string time = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.ff");
+        File.AppendAllText(path, "This study data was collected on: " + time + "\n");
+        for (int i=1; i < scenevalue.Length; i++)
+        { 
+        question[i] = question[i] + " - Scene Number: " + i + "\t";
+        if (LevelLoader.answer[i])
+                answer = "Decision: " + "Yields" + "\n";
+        else
+                answer = "Decision: " + "Doesn't Yield" + "\n";
+        File.AppendAllText(path, question[i] + "\t\t\t" + answer);
+        }
+        File.AppendAllText(path, "\n");
+    }
+
+}
